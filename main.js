6477dbfc825da59e2760b7a79042fe7b6793f726
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-04-07 15:12:33 +0200

    ok

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log('JS OK');
+// console.log('JS OK');
+
+/**
+ * Click su Hamburger menù
+ */
+
+// Ref
+
+var hamb = $('.header-right a i');
+
+var subList = $('.hamburger-menu');
+
+var end = $('.hamburger-menu a.close');
+
+
+
+// Mostrare tendina al click e chiuderla
+
+hamb.click(function(){
+    // alert('click funziona');
+    // subList.show();
+    // subList.fadeIn();
+    subList.slideDown();
+
+});
+
+end.click(function(){
+    // alert('click funziona');
+    // subList.hide();
+    // subList.fadeOut();
+    subList.slideUp();
+
+
+
+})
+
+
+
+
+
